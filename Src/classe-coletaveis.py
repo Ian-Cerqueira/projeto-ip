242ca06a2a397a3ac37f7cb217117c76ef374f46
Ian-Cerqueira <168128013+Ian-Cerqueira@users.noreply.github.com>
2025-03-31 16:06:12 -0300

    Update classe-coletaveis.py

--- Src/classe-coletaveis.py
+++ Src/classe-coletaveis.py
@@ -1,6 +1,7 @@
 import pygame
 from random import randint
 from sys import exit
+from abc import ABC, abstractmethod
 
 pygame.init()
 
@@ -10,17 +11,43 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 BLUE = (0, 0, 255)
 
-class Coletavel:
-    def __init__(self, nome):
-        self.nome = nome
+class Coletavel(ABC):
+    def __init__(self):
         self.valor = randint(10, 50)
         self.x = randint(50, 450)
         self.y = randint(50, 320)
         self.coletado = False
     
+    @abstractmethod
+    def draw(self):
+        pass
+
+
+class moeda(Coletavel) :
+    def __init__(self):
+        super().__init__()
+
     def draw(self):
         if not self.coletado:  
             pygame.draw.rect(screen, (255, 0, 0), (self.x, self.y, 40, 50))
+
+class chave(Coletavel) :
+    def __init__(self):
+        super().__init__()
+    
+    def draw(self):
+        if not self.coletado:  
+            pygame.draw.rect(screen, (255, 255, 0), (self.x, self.y, 40, 50))
+
+class estrela(Coletavel) :
+    def __init__(self):
+        super().__init__()
+    
+    def draw(self):
+        if not self.coletado:  
+            pygame.draw.rect(screen, (0, 255, 255), (self.x, self.y, 40, 50))
+
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -31,7 +58,7 @@ class Player(pygame.sprite.Sprite):
         self.rect.x = screen_width // 2
         self.rect.y = screen_height - 60
         self.velocidade = 5
-        self.inventario = {'moeda': 0}
+        self.inventario = {'moeda': 0, 'chave': 0, 'estrela': 0}
 
     def update(self, keys):
         if keys[pygame.K_LEFT]:
@@ -55,15 +82,21 @@ class Player(pygame.sprite.Sprite):
     def add_moeda(self):
         self.inventario['moeda'] += 1
 
+    def add_chave(self):
+        self.inventario['chave'] += 1
+
+    def add_estrela(self):
+        self.inventario['estrela'] += 1
+
 def verificar_colisao(player, coleta):
     if coleta and not coleta.coletado and player.rect.colliderect(pygame.Rect(coleta.x, coleta.y, 40, 50)):
         return True
     return False
 
 player1 = Player()
-coleta = Coletavel('Ian')   
-coleta2 = Coletavel('Arthur')   
-
+coleta = moeda()  
+coleta2 = chave()   
+coleta3 = estrela()
 
 while True:
     screen.fill('Black')
@@ -88,9 +121,18 @@ while True:
         coleta2.draw()
         if verificar_colisao(player1, coleta2):
             print('Coletou')
-            player1.add_moeda()
+            player1.add_chave()
             print(player1.inventario)
             coleta2 = None 
+
+    if coleta3:
+        coleta3.draw()
+        if verificar_colisao(player1, coleta3):
+            print('Coletou')
+            player1.add_estrela()
+            print(player1.inventario)
+            coleta3 = None 
+
     # Desenha o jogador
     screen.blit(player1.image, player1.rect)
     
